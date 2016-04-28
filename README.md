f9d3091fad4e846698f9c0c89456d49de6a1d41d
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-04-28 04:45:41 -0600

    Chapter 12 - Port to umbrella project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rumbrella
+
+This is the [Elixir][elixir] umbrella project built throughout the book [Programming Phoenix][phoenix].
+
+[elixir]: http://elixir-lang.org/
+[phoenix]: https://pragprog.com/book/phoenix/programming-phoenix
---
+++ apps/rumbl/mix.exs
@@ -0,0 +1,71 @@
+defmodule Rumbl.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :rumbl,
+     version: "0.0.1",
+     build_path: "../../_build",
+     config_path: "../../config/config.exs",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
+     elixir: "~> 1.2",
+     elixirc_paths: elixirc_paths(Mix.env),
+     compilers: [:phoenix, :gettext] ++ Mix.compilers,
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     aliases: aliases,
+     deps: deps]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {Rumbl, []},
+        applications: [
+          :comeonin,
+          :cowboy,
+          :gettext,
+          :info_sys,
+          :logger,
+          :phoenix,
+          :phoenix_ecto,
+          :phoenix_html,
+          :postgrex
+        ]
+     ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
+  defp elixirc_paths(_),     do: ["lib", "web"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      {:comeonin, "~> 2.0"},
+      {:cowboy, "~> 1.0"},
+      {:gettext, "~> 0.9"},
+      {:info_sys, in_umbrella: true},
+      {:phoenix, "~> 1.1.4"},
+      {:phoenix_ecto, "~> 2.0"},
+      {:phoenix_html, "~> 2.4"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:postgrex, ">= 0.0.0"},
+    ]
+  end
+
+  # Aliases are shortcut or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+     "ecto.reset": ["ecto.drop", "ecto.setup"]]
+  end
+end
